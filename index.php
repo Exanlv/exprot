96458e5e4680def4ca3e4e57fcc7da9d9d64e038
Exanlv <exan@landviz.nl>
2026-03-08 15:40:19 +0100

    it worx!

--- index.php
+++ index.php
@@ -9,29 +9,46 @@ use League\Plates\Engine;
 require './vendor/autoload.php';
 
 $resolver = new class implements RowResolver {
+    private readonly PDO $connection;
+
+    public function __construct()
+    {
+        $this->connection = new PDO('sqlite:./source.db');
+    }
+
     public function getRowBatch(int $i): ?array
     {
-        if ($i > 10) {
+        dump($i);
+        $limit = 1000;
+        $offset = $limit * $i;
+
+        $stmt = $this->connection->prepare('SELECT * FROM users LIMIT :limit OFFSET :offset');
+        $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (count($users) === 0) {
             return null;
         }
 
-        $batch = [];
-        for ($rowNum = 1; $rowNum <= 5; $rowNum++) {
-            $batch[] = [
-                "row " . (($i * 5) + $rowNum),                  // row number
-                "row " . (($i * 5) + $rowNum) . " value 2",
-                "row " . (($i * 5) + $rowNum) . " value 3",
-                "row " . (($i * 5) + $rowNum) . " value 4",
-            ];
+        $data = [];
+
+        foreach ($users as $record) {
+            $data[] = array_map(
+                fn (mixed $data) => (string) $data,
+                array_values($record),
+            );
         }
 
-        return $batch;
+        return $data;
     }
 };
 
 $excel = new XlsxFileCreator('./tmp', new Engine(__DIR__ . '/resources/excel'));
 
 $excel->addSheet(new Sheet('rId3', 'Sheet 1', 'sheet1', new SheetWriter($resolver)));
-$excel->addSheet(new Sheet('rId4', 'Sheet 2', 'sheet2', new SheetWriter($resolver)));
+// $excel->addSheet(new Sheet('rId4', 'Sheet 2', 'sheet2', new SheetWriter($resolver)));
 
 $excel->create('test');
